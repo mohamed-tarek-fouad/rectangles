f266921653460b4cf0b5e00cfa1b1bba86d2b7ca
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2023-08-16 19:10:38 +0300

    rectangles task

---
+++ rectangles.js
@@ -0,0 +1,40 @@
+function generateRectanglePositions(areas) {
+  let positions = [];
+
+  let currentPosition = [
+    {
+      width: areas[0],
+      height: areas[0] / areas[0],
+    },
+  ];
+
+  // Recursively generate all possible positions, starting with the first rectangle.
+  function generatePositions(rectanglesLeft, currentPosition) {
+    if (rectanglesLeft <= 1) {
+      // We have reached the end of the list, so add the current position to the results.
+      positions.push(currentPosition);
+    } else {
+      // For each possible width of the next rectangle...
+      for (let width = 1; width <= areas[rectanglesLeft - 1]; width++) {
+        let height = areas[rectanglesLeft - 1] / width;
+
+        let newPosition = currentPosition.slice();
+        newPosition.push({
+          width: width,
+          height: height,
+        });
+
+        // Recursively generate positions for the remaining rectangles.
+        generatePositions(rectanglesLeft - 1, newPosition);
+      }
+    }
+  }
+
+  generatePositions(6, currentPosition);
+  return positions;
+}
+const areas = [1, 2, 3, 4, 5, 6];
+
+const positions = generateRectanglePositions(areas);
+console.log(`the number of alternitaives is ${positions.length}`);
+console.log(positions);
